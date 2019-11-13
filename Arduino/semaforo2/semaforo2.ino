1f92df05a1c3205ffa7eadb797862a5d64d4edf4
Mortallica <41587633+Mortallica@users.noreply.github.com>
2019-11-13 10:50:11 -0500

    Update semaforo2.ino

--- Arduino/semaforo2/semaforo2.ino
+++ Arduino/semaforo2/semaforo2.ino
@@ -1,6 +1,7 @@
 https://github.com/BehindTheSciences/Raspberry-Pi-Bluetooth-GPIO-Control
 https://www.instructables.com/id/Control-Raspberry-Pi-GPIO-Using-an-App/
 https://www.instructables.com/id/Control-Arduino-using-android-app/#step5
+https://github.com/Mortallica/Universidad/blob/master/Arduino/semaforo2/semaforo2.ino
 
 
 
